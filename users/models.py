be54bdc8bdec758b4614ec09226ce29657f78ba1
ersan80 <ersan.kuleli80@gmail.com>
2022-05-30 19:20:03 +0300

    auth2

---
+++ users/models.py
@@ -0,0 +1,11 @@
+from distutils.command.upload import upload
+from re import T
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+    portfolio = models.URLField(blank=True)
+    profile_pic = models.ImageField(upload_to="profile_pics",blank=True)
+    username = models.EmailField("Email Address",unique=True)
+    REQUIRED_FIELDS = []
+
